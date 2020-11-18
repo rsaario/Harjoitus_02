16010c693b670020b055142d09b26b154748a0d1
Roope Saario <roope.saario@ictpro.labra>
2020-11-18 11:35:15 +0200

    Varmistettu kytkennän toimivuus

--- Harjoitus_02.ino
+++ Harjoitus_02.ino
@@ -4,8 +4,17 @@
 
 void setup(){
   pinMode(LED, OUTPUT);
+  Serial.begin(9600);
 }
+
+int viive = 1000;
 
 void loop(){
   digitalWrite(LED, LEDON);
+  Serial.println("Syttyy");
+  delay(viive);
+  digitalWrite(LED, LEDOFF);
+  Serial.println("sammuu");
+  delay(viive);
+  
 }
